1e6c94a0e1b549106974a218305d7e1841a4da91
Saghan Mudbhari <samudbha@microsoft.com>
2020-07-25 16:06:43 -0700

    work in progess 1

--- Breathe/Form1.Designer.cs
+++ Breathe/Form1.Designer.cs
@@ -28,13 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(224, 79);
+            this.trackBar1.Maximum = 60;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(349, 45);
+            this.trackBar1.SmallChange = 5;
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 2;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "5 minutes";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(526, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "60 minutes";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(302, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Time interval for reminder";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(338, 198);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 352);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button StartButton;
     }
 }
 
--- Breathe/Form1.cs
+++ Breathe/Form1.cs
@@ -6,15 +6,107 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
 using System.Windows.Forms;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+using System.Diagnostics;
 
 namespace Breathe
 {
     public partial class Form1 : Form
     {
+        private static System.Timers.Timer aTimer;
+        private static int interval = 1;
+        private static ToastNotifier toastNotifier;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int val = trackBar1.Value;
+            int interval = trackBar1.Value;
+
+            string msg = String.Format("Time interval for reminder: {0} minutes", val);
+            this.textBox1.Text = msg;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void RepeatingEvent()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(interval*1000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            //aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            aTimer.Start();
+
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+           
+
+            //Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+            //e.SignalTime);
+
+            //var toast_text_arr = System.IO.File.ReadLines(Resource1.quotes).ToArray();
+            var toast_text_arr = Resource1.quotes.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            string toastXmlString =
+$@"<toast><visual>
+            <binding template='ToastGeneric'>
+            <text>toast title</text>
+            <text>2</text>
+            </binding>
+        </visual></toast>";
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(toastXmlString);
+            var toastNotification = new ToastNotification(xmlDoc);
+            toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            toastNotifier.Show(toastNotification);
+            
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+            RepeatingEvent();
+
+        }
+
+        //private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        //{
+        //    Debug.WriteLine("xxxxxxxx closed");
+        //}
+        override
+        protected void OnFormClosing(System.Windows.Forms.FormClosingEventArgs e)
+        {
+            Debug.WriteLine("xxxxxxxx closed");
+            aTimer.Enabled = false;
+            aTimer.Stop();
+            aTimer.Dispose();
+            
+            //System.Windows.Application.Current.Shutdown();
+            //System.Diagnostics.Process.GetCurrentProcess().Kill();
+            //Environment.Exit(0);
+
+
+        }
+
     }
 }
